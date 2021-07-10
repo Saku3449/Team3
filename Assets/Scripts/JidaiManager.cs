f1971d6fc8cd4876ac29f19d8ceffdd54df82019
入星敦輝 <atsuki@AtsukinoMacBook-Air.local>
2021-07-10 18:00:54 +0900

    Scene

---
+++ Assets/Scripts/JidaiManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+
+public class JidaiManager : MonoBehaviour
+{
+    [SerializeField]    	
+    private GameObject manager_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TimeNext()
+    {
+        Text selectedBtn = button.GetComponentInChildren<Text> ();
+		if (selectedBtn.text == "江戸時代") {
+            SceneManager.LoadScene("SelectDifficulty");
+        }
+        
+    }
+}
